2452b6afcc3352c769f42e29f9b84357440697ff
SiqMello <91890617+Siqm@users.noreply.github.com>
2022-09-27 13:44:31 -0300

    Configuração da database, criação da tabela e inicio de migrações

--- Changelog.md
+++ Changelog.md
@@ -1,2 +1,3 @@
 # Primeiro commit, instalação realizada
 
+# Segundo commit, Configuração da database, criação da tabela e inicio de migrações
--- src/config/database.js
+++ src/config/database.js
@@ -1,4 +1,12 @@
-const express = require('express')
-
-const routes = express.Router();
-
+module.exports = {
+    dialect:'postgres',
+    host:'localhost',
+    username:'postgres',
+    password:'4adL#iKA 91maA',
+    database:'Implementando_Postgres',
+    ssl: true,
+    define: {
+        timestamps: true,
+        underscored: true,
+    }
+}
